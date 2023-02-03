cc79410c62ac3d2db2505fa57eb3b4e8f998999d
박찬현 <chkfrdlek@naver.com>
2023-02-03 10:30:42 +0900

    #1 - Querydsl 지원 이용 api 검색옵션 추가,gitignore 추가
    QuerdslPredicateExecutor,
    QuerydslBinderCustomizer 를 이용하여 간단하게 개발

--- project-board/src/main/java/fc/projectboard/repository/ArticleCommentRepository.java
+++ project-board/src/main/java/fc/projectboard/repository/ArticleCommentRepository.java
@@ -1,10 +1,31 @@
 package fc.projectboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import fc.projectboard.domain.Article;
 import fc.projectboard.domain.ArticleComment;
+import fc.projectboard.domain.QArticle;
+import fc.projectboard.domain.QArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ArticleCommentRepository extends
-        JpaRepository<ArticleComment, Long> { //검색기능
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>, //검색기능
+        QuerydslBinderCustomizer<QArticleComment> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);//
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);//
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);//
+    }
+
 }
--- project-board/src/main/java/fc/projectboard/repository/ArticleRepository.java
+++ project-board/src/main/java/fc/projectboard/repository/ArticleRepository.java
@@ -1,10 +1,33 @@
 package fc.projectboard.repository;
 
+import com.querydsl.core.types.dsl.DateExpression;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import fc.projectboard.domain.Article;
+import fc.projectboard.domain.QArticle;
+import fc.projectboard.domain.QArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ArticleRepository extends
-        JpaRepository<Article, Long>{
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {//기본 검색기능 추가해줌{
+
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);//
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);//
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);//
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);//
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);//
+    }
 }
