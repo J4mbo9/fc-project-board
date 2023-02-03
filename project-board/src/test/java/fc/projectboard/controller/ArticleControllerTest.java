13ce690ed5d8daf2a1d38145ec88921209bf9e58
박찬현 <chkfrdlek@naver.com>
2023-02-03 11:36:32 +0900

    #16 - 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세 뽑음

---
+++ project-board/src/test/java/fc/projectboard/controller/ArticleControllerTest.java
@@ -0,0 +1,68 @@
+package fc.projectboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)// 입력된 클래스만 테스트함 - 가볍게 테스트 가능
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상호출")
+    @Test
+    public void givenNothing_whenRequestArticlesView_thenReturnArticlesView() throws Exception {
+        //given
+
+        //when&then
+        mvc.perform(MockMvcRequestBuilders.get("/articles"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("articles"));
+    }
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상호출")
+    @Test
+    public void givenNothing_whenRequestArticleView_thenReturnArticlesView() throws Exception {
+        //given
+
+        //when&then
+        mvc.perform(MockMvcRequestBuilders.get("/articles/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("articles"));
+    }
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상호출")
+    @Test
+    public void givenNothing_whenRequestArticleSearchView_thenReturnArticlesView() throws Exception {
+        //given
+
+        //when&then
+        mvc.perform(MockMvcRequestBuilders.get("/articles/search"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML));
+    }
+
+    @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상호출")
+    @Test
+    public void givenNothing_whenRequestArticleHashtagSearchView_thenReturnArticlesView() throws Exception {
+        //given
+
+        //when&then
+        mvc.perform(MockMvcRequestBuilders.get("/articles/search-hashtag"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML));
+    }
+
+}
--- project-board/src/test/java/fc/projectboard/controller/DataRestTest.java
+++ project-board/src/test/java/fc/projectboard/controller/DataRestTest.java
@@ -1,4 +1,4 @@
-package fc.projectboard.repository;
+package fc.projectboard.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
